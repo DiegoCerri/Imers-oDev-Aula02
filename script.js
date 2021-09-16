a7186be67f0189a28ead68427dcf88ebe2aa5b8f
Diego Cerri <90693728+DiegoCerri@users.noreply.github.com>
2021-09-15 22:54:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+var data = new Date();
+var mes = data.getMonth();
+var dia = data.getDay();
+var ano = data.getFullYear();
+var dataCompleta = mes + "-" + dia + "-" + ano;
+
+var mURL = `https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='${dataCompleta}'&$format=json`;
+
+function reqListener() {
+  var resposta = JSON.parse(this.responseText);
+  var valores = resposta.value[0];
+  document.getElementById("dolar").innerHTML = valores.cotacaoCompra;
+}
+
+var oReq = new XMLHttpRequest();
+oReq.addEventListener("load", reqListener);
+oReq.open("GET", mURL);
+oReq.send();
+
+function Converter() {
+  var real = Number.parseFloat(document.getElementById("valorR").value);
+  var dolar = Number.parseFloat(document.getElementById("valorD").value);
+  var cambio = document.getElementById("dolar").innerHTML;
+
+  /*  if (real > "0") {
+    document.getElementById("valorD").value = (real / cambio).toFixed(2);
+  } else {
+    document.getElementById("valorR").value = (dolar * cambio).toFixed(2);
+  }
+  */
+}
+
+function Limpar() {
+  document.getElementById("valorD").value = null;
+  document.getElementById("valorR").value = null;
+}
